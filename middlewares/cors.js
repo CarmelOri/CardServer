c6c4fc8554823710ca3ec708e8176ffcbc5f1202
CarmelOri <ori3carmel@gmail.com>
2024-10-28 10:26:38 +0200

    add website to the cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,13 @@
 const cors = require("cors")
 
 const corsMiddleware = cors({
-    origin: ["http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://localhost:5173", "https://cards2project.onrender.com", "https://cardserver-6vas.onrender.com"]
+    origin: [
+        "http://127.0.0.1:5500",
+        "http://127.0.0.1:5501",
+        "http://localhost:5173",
+        "https://cardsproject-g9se.onrender.com",
+        "https://cardserver-6vas.onrender.com"
+    ]
 })
 
 module.exports = corsMiddleware;
